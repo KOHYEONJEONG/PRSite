894d982ffbf91f299543253f2c52e7a422e8da8d
ko <080821@naver.com>
2021-12-02 01:32:54 +0900

    서버시수정

--- PRSite/src/main/java/prsite/spring/member/service/MemberJoinService.java
+++ PRSite/src/main/java/prsite/spring/member/service/MemberJoinService.java
@@ -22,14 +22,17 @@ public class MemberJoinService implements IMemberService {
 		HttpServletRequest request = (HttpServletRequest)map.get("request");
 		
 		
+		
 		String id = request.getParameter("id");
 		String pwd = request.getParameter("pwd");
 		String influyn = request.getParameter("influyn");
 		String name = request.getParameter("nickname");
 		
+
+		
 		
 		if(id==null || pwd==null|| influyn==null || name==null) {
-			System.out.println("<script>alert('입력해주세요.');</script>");
+			System.out.println("로그인오류)입력 안됨.");
 		}else {
 			MemberDto member =new  MemberDto(id ,pwd,influyn, name);
 			MemberDao dao = new MemberDao();
--- PRSite/src/main/java/prsite/spring/member/service/MemberloginService.java
+++ PRSite/src/main/java/prsite/spring/member/service/MemberloginService.java
@@ -26,9 +26,9 @@ public class MemberloginService implements IMemberService {
 		//로그인되면 true, false 반환됨
 		boolean result = dao.memberLogin(id, pwd);
 		if(result==true) {
-			System.out.println("<script>alert(로그인성공);</script>");
+			//out.println("<script>alert(로그인성공);</script>");
 		}else {
-			System.out.println("<script>alert(로그인실패);</script>");
+			//out.println("<script>alert(로그인실패);</script>");
 		}
 		
 	}

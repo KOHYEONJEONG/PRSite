f24feac58bf4b4a868c17a690450ce12c9d24ecf
ycs-201807005 <leen3357@gmail.com>
2021-12-02 01:22:29 +0900

    [*] Influencer Services, InfluencerDao influencerSearch()

--- PRSite/src/main/java/prsite/spring/influencer/service/InfluencerRcmmService.java
+++ PRSite/src/main/java/prsite/spring/influencer/service/InfluencerRcmmService.java
@@ -1,13 +1,20 @@
 package prsite.spring.influencer.service;
 
+import java.util.ArrayList;
+
 import org.springframework.ui.Model;
+
+import prsite.spring.dto.InfluencerDto;
+import prsite.spring.influencer.dao.InfluencerDao;
 
 public class InfluencerRcmmService implements IInfluencerService {
 //인플루언서 구독자수 추천
 	
 	@Override
 	public void execute(Model model) {
-		// TODO Auto-generated method stub
+		InfluencerDao influencerDao =  new InfluencerDao();
+		ArrayList<InfluencerDto> dtos = influencerDao.influencerRcmm();
+		model.addAttribute("rcmmlist", dtos);
 
 	}
 
